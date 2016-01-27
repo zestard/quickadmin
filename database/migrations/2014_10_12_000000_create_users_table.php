57c6272c6165a1d3119067355ea0c52b8f9a5a34
zestard <test.zestard@gmail.com>
2016-01-27 17:22:06 +0530

    file updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->rememberToken();
